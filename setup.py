8236a16c11cefef3077c2a871d94754bb999c4b4
Oscar <oscar.s.kremer@hotmail.com>
2020-05-03 21:10:41 -0300

    First commit: Add cookiecutter datascience template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code for modelling, control and optimization of a single joint flexible arm-like robot',
+    author='Oscar Schmitt Kremer',
+    license='MIT',
+)
